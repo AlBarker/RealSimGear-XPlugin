2237af7aa227b311e967920423541106352d7761
Jarrod Barker <jarrod.barker@gmail.com>
2018-05-20 21:42:25 -0700

    Code that opens COM3 and displays serial messages sent by the Arduino

---
+++ Com port listener/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+public class PortChat
+{
+    static bool _continue;
+    static SerialPort _serialPort;
+
+    public static void Main()
+    {
+        string name;
+        string message;
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort("COM3", 9600);
+
+       
+       // Set the read/write timeouts
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+        _continue = true;
+        readThread.Start();
+        
+        //Console.WriteLine("Type QUIT to exit");
+
+        while (_continue)
+        {
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                _serialPort.WriteLine(
+                    String.Format("<{0}>: {1}", message));
+            }
+        }
+
+        readThread.Join();
+        _serialPort.Close();
+    }
+
+    public static void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine(message);
+            }
+            catch (TimeoutException) { }
+        }
+    }
+}
